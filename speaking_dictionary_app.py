b84d0bdb6edd8c3d2272e2a31c7658e4d8b7f9eb
prashantepicprogrammer <61360799+prashantepicprogrammer@users.noreply.github.com>
2020-04-17 10:27:02 -0700

    Add files via upload

---
+++ speaking_dictionary_app.py
@@ -0,0 +1,21 @@
+import requests
+import bs4
+from gtts import gTTS
+import playsound
+
+search = input('Search Anything : ')
+
+def speech(msg):
+    language = 'en'
+    msgobj = gTTS(text=msg , lang=language , slow=False)
+    msgobj.save('speech.mp3')
+    playsound.playsound('speech.mp3' , True)
+
+def fetchData(search):
+    res = requests.get('https://en.wikipedia.org/wiki/'+search)
+    soup = bs4.BeautifulSoup(res.text , 'lxml')
+    data = soup.select('p')
+    print(data[1].text)
+    speech(data[1].text)
+
+
